7864a945cd1e649aa9b29486548bfc94e61d2b56
dani-sj <aliabuotorabi@gmail.com>
2016-02-23 01:57:43 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {172630253},--Sudo users
+	    sudo_users = {103365027,173345972,121135478,24878907,126477037},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
